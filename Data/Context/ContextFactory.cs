135a491d56066be59bc119ac889fd3522b5bd1bc
leona <leona@LEONARDO>
2023-12-05 00:20:34 -0300

    Add project files.

---
+++ Data/Context/ContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            var connectionString = "server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=12345";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseMySql(connectionString);
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
