01e2ef81c0e1ba8e138656f564f92b7a7faf521e
Leonardo da Silva Pipoli <leonardo.pipoli48@outlook.com>
2023-12-08 10:43:50 -0300

    correção da versão

--- API/Startup.cs
+++ API/Startup.cs
@@ -21,7 +21,7 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             ConfigureService.ConfigureDependenciesService(services);
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
